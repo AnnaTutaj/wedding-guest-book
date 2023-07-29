08cc6020837733097d3b06193ada47b025d63ad3
AnnaTutaj <anna.tutaj@outlook.com>
2023-07-29 11:49:07 +0200

    Counter: adjust width of values: - to prevent a little "jumping" when two digits turn to one digit (eg. 10-> 9)

--- src/common/components/Countdown/CountdownValueDisplay/styled.ts
+++ src/common/components/Countdown/CountdownValueDisplay/styled.ts
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
   text-align: center;
+  min-width: 63px;
+
+  @media (width <= 360px) {
+    min-width: 42px;
+  }
 `;
 
 export const StyledValue = styled.div`
